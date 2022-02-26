99b265c075c4d6bc45d733b6d2b0bfd6e37d8564
simplon-romainB <neriaa@hotmail.fr>
2022-02-26 12:54:31 +0100

    init gesture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SocialGesturesDirective } from './social-gestures.directive';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, SocialGesturesDirective],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
---
+++ src/app/social-gestures.directive.spec.ts
@@ -0,0 +1,8 @@
+import { SocialGesturesDirective } from './social-gestures.directive';
+
+describe('SocialGesturesDirective', () => {
+  it('should create an instance', () => {
+    const directive = new SocialGesturesDirective();
+    expect(directive).toBeTruthy();
+  });
+});
